64cc278242a70e048319de1c640334005424772c
motoZ-crypto <motoz.crypto@proton.me>
2026-09-21 09:18:22 +0000

    Redirect dead wallet /tx and /address links to the EVM pages

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  redirects: () => [
+    { source: "/tx/:hash", destination: "/evm/tx/:hash", permanent: true },
+    { source: "/address/:address", destination: "/evm/address/:address", permanent: true },
+  ],
 };
 
 export default nextConfig;
